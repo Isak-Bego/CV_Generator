ae2da79666b92b776fbc230e60a53fe7bde92e01
Isak Bego <isakbego@gmail.com>
2024-01-25 00:23:13 +0200

    created the logic for the data controller

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CV Generator
 Description </br>
-This project is part of the React Course offered by the Odin Project. It is a proof of concept for creating the first react app, using the components, and the useState hook.
+This project is part of the React Course offered by the Odin Project. It is a proof of concept for creating the first react app, using the components, and the useState hook.``
---
+++ src/dataController.js
@@ -0,0 +1,57 @@
+let fullData; 
+
+export function initialize() { 
+    //Establish the data point in the local storage if one such doesn't exits
+    if (!localStorage.getItem("cvGeneratorData")) {
+      fullData = {
+        personalInformation: {
+          name: "",
+          birthday: "",
+          nationality: "",
+          email: "",
+          phoneNumber: "",
+        },
+        educationalExperience: [],
+        practicalExperience: [],
+      };
+      localStorage.setItem("cvGeneratorData", JSON.stringify(fullData));
+    }else{
+        fullData = JSON.parse(localStorage.getItem("cvGeneratorData"));
+    }
+  }
+
+export function getPersonalInformation() {
+    return JSON.parse(localStorage.getItem("cvGeneratorData")).personalInformation;
+  }
+
+export function setPersonalInformation(updatedObject){
+    fullData.personalInformation = updatedObject; 
+    localStorage.setItem("cvGeneratorData", JSON.stringify(fullData));
+  }
+
+export function getEducationalExperience(){
+    return JSON.parse(localStorage.getItem("cvGeneratorData")).educationalExperience;
+  }
+
+export function setEducationalExperience(updatedArray){
+    fullData.educationalExperience = updatedArray; 
+    localStorage.setItem("cvGeneratorData", JSON.stringify(fullData));
+  }
+
+export function getPracticalExperience(){
+    return JSON.parse(localStorage.getItem("cvGeneratorData")).practicalExperience;
+  }
+
+export function setPracticalExperience(updatedArray){
+    fullData.practicalExperience = updatedArray; 
+    localStorage.setItem("cvGeneratorData", JSON.stringify(fullData));
+  }
+
+export function getAllData(){
+    return fullData;
+  }
+
+export function setAllData(updatedObject){
+    fullData = updatedObject; 
+    localStorage.setItem("cvGeneratorData", JSON.stringify(fullData));
+  }
