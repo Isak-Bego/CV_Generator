96cbe0e48b7d89e03e92e3a444628796e56349db
Isak Bego <isakbego@gmail.com>
2024-01-25 07:07:45 +0200

    implements Editor v1.0

--- src/components/EducationDetails.jsx
+++ src/components/EducationDetails.jsx
@@ -1,13 +1,25 @@
+import { getEducationalExperience, setEducationalExperience } from '../dataController';
 import '../styles/Experience.css'
 
-function EducationDetails({educationXP}){
+function EducationDetails({educationXP, triggerReRender}){
     return(
         educationXP.map((education) => {
             return (
-                <div className="details experience" key={education.schoolName}>
-                    <h5 className="infoField"><span>School Name</span><br></br> {education.schoolName}</h5>
-                    <h5 className="infoField"><span>Area of Study</span><br /> {education.areaOfStudy}</h5>
-                    <h5 className="infoField">{`${education.startDate} to ${education.endDate}`}</h5>
+                <div className='experience' key={education.schoolName}>
+                    <div className="details">
+                        <h5 className="infoField"><span>School Name</span><br></br> {education.schoolName}</h5>
+                        <h5 className="infoField"><span>Area of Study</span><br /> {education.areaOfStudy}</h5>
+                        <h5 className="infoField">{`${education.startDate} to ${education.endDate}`}</h5>
+                    </div>
+                    <div className="deleteIconHolder" onClick={ () => {
+                        educationXP = getEducationalExperience().filter((educationX) => {
+                                        return educationX.schoolName !== education.schoolName;
+                                    });
+                        triggerReRender();
+                        setEducationalExperience(educationXP);}
+                    }>
+                        <img src="../../public/delete-button.svg" alt="delete" />
+                    </div>
                 </div>
             );
         })
--- src/components/WorkDetails.jsx
+++ src/components/WorkDetails.jsx
@@ -1,13 +1,27 @@
-function WorkDetails({workXP}){
+import '../styles/Experience.css'
+import { getPracticalExperience, setPracticalExperience } from '../dataController';
+
+function WorkDetails({workXP, triggerReRender}){
     return(
         workXP.map((work) => {
             return (
-                <div className="details experience" key={work.companyName}>
+            <div className="experience" key={work.companyName}>
+                <div className="details" >
                     <h5 className="infoField"><span>Company Name</span><br></br> {work.companyName}</h5>
                     <h5 className="infoField"><span>Position</span><br /> {work.position}</h5>
                     <h5 className="infoField"><span>Description</span><br /> {work.description}</h5>
                     <h5 className="infoField">{`${work.startDate} to ${work.endDate}`}</h5>
                 </div>
+                <div className="deleteIconHolder" onClick={ () => {
+                        workXP = getPracticalExperience().filter((workX) => {
+                                        return workX.companyName !== work.companyName;
+                                    });
+                        triggerReRender();
+                        setPracticalExperience(workXP);}
+                    }>
+                    <img src="../../public/delete-button.svg" alt="delete" />
+                </div>
+            </div>
             );
         })
     );
